2dc4c6c1cf4457ccedf657cbf00b0b74f05bd15c
imgurbot12 <imgurbot12@gmail.com>
2023-02-27 20:13:18 -0700

    fix: added tsconfig and fixed ts

--- src/ts/sidebar.ts
+++ src/ts/sidebar.ts
@@ -1,18 +1,19 @@
 
 /* Variables */
 
-const HIDE     = "sidebar-hide"
 const COLLAPSE = "sidebar-collapse"
 
 /* Functions */
 
-function initCollapseButton(collapse: object) {
+function initCollapseButton(collapse: HTMLElement | null) {
   if (!collapse) { return }
   collapse.addEventListener('click', event => {
     event.preventDefault()
     // find sidebar and toggle collapse class
     const sidebar = document.querySelector('.sidebar')
-    sidebar.classList.toggle(COLLAPSE)
+    if (sidebar) {
+      sidebar.classList.toggle(COLLAPSE)
+    }
   });
 }
 
@@ -22,3 +23,5 @@ window.addEventListener('DOMContentLoaded', () => {
   const collapse = document.getElementById('sidebarCollapse')
   initCollapseButton(collapse)
 });
+
+export {}
